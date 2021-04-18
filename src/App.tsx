65d7948ed39ac0592d7e5caf10ae25d1125c1cc7
Rennan Ribas <rennanrr@hotmail.com>
2021-04-18 18:59:17 -0300

    [notification] websocket and notifications

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,9 @@
-import { useEffect, useState } from 'react';
-import { DefaultEventsMap } from 'socket.io-client/build/typed-events';
 import './App.css';
-import Api from './services/api';
-import { initiateSocket, disconnectSocket,
-  subscribeToChat } from './services/socket';
+import MessageComponent from './Components/Message.component';
 
-const App = () => {
-  const [ stringOne, setStringOne ] = useState<string>("");
-  const [ stringTwo, setStringTwo ] = useState<string>("");
-  const rooms = ['A', 'B', 'C'];
-  const [room, setRoom] = useState(rooms[0]);
-  const [message, setMessage] = useState('');
-  const [chat, setChat] = useState([]);
-
-  const handleSend = async () => {
-    await Api.post('/alert', {stringOne, stringTwo});
-    console.log(stringOne + stringTwo);
-  };
-
-  useEffect(() => {
-    if (room) initiateSocket(room);
-    subscribeToChat();
-    return () => {
-      disconnectSocket();
-    }
-  }, [room]);
-
+function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <input placeholder="String one" onChange={e => setStringOne(e.target.value)}/>
-        <input placeholder="String two"onChange={e => setStringTwo(e.target.value)}/>
-        
-        <button onClick={handleSend}>Submit strings</button>
-      </header>
-    </div>
+    <MessageComponent></MessageComponent>
   );
 }
 
--- src/services/socket.ts
+++ src/services/socket.ts
@@ -1,21 +1,17 @@
 import io, { Socket } from 'socket.io-client';
+import notifyMe from '../Utils/notifications';
 let socket: Socket;
-export const initiateSocket = (room: string) => {
-  socket = io('ws://localhost:4555');
-  console.log(`Connecting socket...`);
-  if (socket && room) socket.emit('join', room);
+export const initiateSocket = () => {
+  socket = io('http://localhost:4555');
+  if (socket) socket.emit('join');
 }
 export const disconnectSocket = () => {
-  console.log('Disconnecting socket...');
   if(socket) socket.disconnect();
 }
-export const subscribeToChat = () => {
+export const subscribeToChat = (methods: () => void) => {
   if (!socket) return(true);
-  return socket.on('chat', msg => {
-    console.log('Websocket event received!');
-    console.log(msg);
+  return socket.on('message', msg => {
+    notifyMe(msg);
+    methods();
   });
-}
-export const sendMessage = (room: string, message: string) => {
-  if (socket) socket.emit('chat', { message, room });
 }
